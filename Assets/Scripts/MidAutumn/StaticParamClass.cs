57ad1387569de5e881e30ee623f517e3a08ee9a8
kien.nguyen3 <kien.nguyen3@ekoios.vn>
2023-09-09 10:33:13 +0700

    set username

--- Assets/Scripts/MidAutumn/StaticParamClass.cs
+++ Assets/Scripts/MidAutumn/StaticParamClass.cs
@@ -6,4 +6,7 @@ public static class StaticParamClass
 	public static string CheckinNumber { get; set; }
 
 	public static int CheckinPlace { get; set; }
+
+	public static string PrefCheckinName = "CheckinName";
+	public static string PrefCheckinNumber = "CheckinNumber";
 }
