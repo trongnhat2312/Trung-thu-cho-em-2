04d8575a4e952084886c5251eb992d571317bd7e
mnx293_macpro14 <minhnx293@gmail.com>
2025-10-04 07:36:10 +0700

    fix: bug Completed to unlock star3D

--- Assets/Scenes/MidAutumn/MidAutumnMainSceneController.cs
+++ Assets/Scenes/MidAutumn/MidAutumnMainSceneController.cs
@@ -1,12 +1,24 @@
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MidAutumnMainSceneController : MainController
 {
     [SerializeField] StarLightTransformer starLightTransformer;
+    [SerializeField] Text txtComplete;	
+    protected bool isAnimCompleted = false;
 
 
-    protected override void ShowVictoryUI()
+    protected override async void ShowVictoryUI()
     {
-        starLightTransformer.DoTransformToStarLight();
+        if (!isAnimCompleted)
+        {
+
+            isAnimCompleted = true;
+            starLightTransformer.DoTransformToStarLight();
+            await UniTask.Delay(2000);
+            txtComplete.gameObject.SetActive(true);
+        }
     }
 }
